52a6ac647e1103442134c10904ae4476e2ddc490
qwer9545 <rkddnjs723@gmail.com>
2024-02-20 16:49:10 +0900

    Updata build file

--- src/test/java/edu/fisa/lab/Step07CitestApplicationTests.java
+++ src/test/java/edu/fisa/lab/Step07CitestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Step06CitestApplicationTests {
+class Step07CitestApplicationTests {
 
 	@Test
 	void contextLoads() {
